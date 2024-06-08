141039a3ac06a3993c0f1b2097e01fa7ed17f367
Vancheg <manshin9300@gmail.com>
2024-06-08 18:01:52 +0300

    Add files via upload

---
+++ ServiceLocator/ServiceLocator.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Internal.Codebase.ServiceLocator
+{
+    public static class ServiceLocator
+    {
+        public static Dictionary<Type, IService> Services;
+
+        public static void Register<TService>(TService service) where TService : IService
+        {
+            if (Services.ContainsKey(typeof(TService)))
+            {
+                Debug.Log("This service has already been registered ");
+                return;
+            }
+            
+            Services.Add(typeof(TService), service);
+        }
+
+        public static void Deregister<TService>() where TService : IService
+        {
+            if (!Services.ContainsKey(typeof(TService)))
+            {
+                Debug.Log("This service is not registered");
+                return;
+            }
+            
+            Services.Remove(typeof(TService));
+        }
+
+        public static IService GetService<TService>() where TService : IService
+        {
+            if (!Services.ContainsKey(typeof(TService)))
+            {
+                Debug.Log("This service is not registered");
+                return null;
+            }
+
+            return (TService)Services[typeof(TService)];
+        }
+    }
+}
